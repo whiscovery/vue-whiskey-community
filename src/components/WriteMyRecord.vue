0e49020f4be12576aaab5f1fdca2c7390553fd8b
whiscovery <whiscovery@gmail.com>
2021-05-17 23:50:45 +0900

    개인기록 쓰기

---
+++ src/components/WriteMyRecord.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+  <div>Recrod</div>
+  <form class="row g-3 mt-3">
+    <div class="col-md-6 form-floating mb-3">
+      <input type="text" v-model="마신술" class="form-control" id="이름" placeholder="이름">
+      <label class="label" for="이름">마신술</label>
+    </div>
+    <div class="col-md-3 form-floating mb-3">
+      <input type="text" v-model="장소" class="form-control" id="장소" placeholder="장소">
+      <label class="label" for="장소">장소</label>
+    </div>
+    <div class="col-md-3 form-floating mb-3">
+      <datepicker v-model="일시" format='yyyy-MM-dd' input-class="form-control pb-4" id="일시" />
+      <label class="label" for="일시">마신 날<br /></label>
+    </div>
+  <div class="form-floating">
+  <textarea v-model="내용" class="form-control" placeholder="Leave a comment here" id="코멘트" style="height: 120px"></textarea>
+    <label class="label" for="코멘트">음주 기록</label>
+  </div>
+  <div class="btnmodi">
+    <button type="submit" class="btn btn-warning mt-3">코멘트 입력</button><button @click="$emit('closeModal')" type="submit" class="btn btn-success ms-3 mt-3">창닫기</button>
+  </div>
+
+  </form>
+</div>
+</template>
+
+<script>
+import Datepicker from 'vuejs-datepicker'
+
+export default {
+  name: 'WriteMyRecord',
+  components: {
+    Datepicker: Datepicker
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/MyWorld.vue
+++ src/views/MyWorld.vue
@@ -1,23 +1,42 @@
 <template>
   <div>
-      <h1> My World </h1>
+    <transition name="myModal">
+    <div v-if="commentwriteModal" class="black-bg">
+        <div class="white-bg">
+        <WriteMyRecord @closeModal="commentwriteModal=false" />
+        </div>
+    </div>
+    </transition>
+
+      <h2 class="mt-5"> 개인기록 </h2>
+      <div class="button-write"><button @click="commentwriteModal=true" class="btn btn-warning">기록하기</button></div>
       <div class="container mt-3">
         <div class="row">
-            <div class="col-sm-3 text-left">
+            <div class="col-sm-2 text-left">
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">음주 기록</li>
-                    <li class="list-group-item">A second item</li>
-                    <li class="list-group-item">A third item</li>
+                    <li class="list-group-item"></li>
                 </ul>
             </div>
-            <div class="col-sm-9">{{comments}}
-                <div v-for="comment, i in comments" :key="i" class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-                    <div class="card-header">{{comments[i].일시}} ({{comments[i].장소}}</div>
-                    <div class="card-body">
-                        <h5 class="card-title">술이름</h5>
-                        <p class="card-text">{{comments[i].내용}}</p>
-                    </div>
-                </div>
+            <div class="col-sm-10">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                        <th class="col-sm-1 table-text">마신날</th>
+                        <th class="col-sm-1 table-text">마신곳</th>
+                        <th class="col-sm-3 table-text">술이름</th>
+                        <th class="col-sm-7 table-text">내용</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="comment, i in comments" :key="i" >
+                        <td class="table-text">{{comments[i].일시}}</td>
+                        <td class="table-text">{{comments[i].장소}}</td>
+                        <td class="table-text">{{comments[i].위스키이름}}</td>
+                        <td class="table-text">{{comments[i].내용}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
         </div>
@@ -26,9 +45,13 @@
 
 <script>
 import axios from 'axios'
+import WriteMyRecord from '@/components/WriteMyRecord'
 
 export default {
   name: 'MyWorld',
+  components: {
+    WriteMyRecord: WriteMyRecord
+  },
   data () {
     return {
       totals: [],
@@ -37,7 +60,8 @@ export default {
         nickname: '진욱',
         email: 'rootack@gmail.com',
         password: 'qwer1234'
-      }
+      },
+      commentwriteModal: false
     }
   },
   created () {
@@ -54,6 +78,68 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.button-write{
+    text-align: right;
+}
+.table-text{
+    font-size: 13px;
+    text-align: left;
+}
+.black-bg {
+  width : 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.8);
+  position: fixed;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+}
+.title{
+  font-family: 'Exo 2', sans-serif;
+}
+.white-bg {
+  color: #000;
+  top: 50%;
+  left: 50%;
+  width: 60%;
+  background: #fff;
+  border-radius: 8px;
+  padding: 20px;
+  z-index: 3;
+  font-weight: 800;
+}
+.white-bg .contact-us-title{
+  width:100%;
+  color: #fff;
+  background: darkslateblue;
+}
+@media(max-width: 720px){
+  .black-bg {
+    width : 100%;
+    height: 100%;
+    background: rgba(0,0,0,0.8);
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 2;
+  }
+  .title{
+    font-family: 'Exo 2', sans-serif;
+  }
+  .white-bg {
+    color: #000;
+    margin: 0px;
+    top: 50%;
+    left: 50%;
+    width: 100%;
+    background-color: #fff;
+    border-radius: 8px;
+    z-index: 3;
+    font-size: 14px;
+  }
+}
 </style>

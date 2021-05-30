1180fa31c608715a21c86e4014c700db8ccb0461
whiscovery <whiscovery@gmail.com>
2021-05-30 22:07:40 +0900

    .

--- src/components/Listitem.vue
+++ src/components/Listitem.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="card shadow-sm">
-            <img :src="whiskey.이미지" class="thumbnail-whiskey">
+            <router-link class="text-link" :to="{ name: 'Detail', params: { id: whiskey.id }}"><img :src="whiskey.이미지" class="thumbnail-whiskey"></router-link>
 
             <div class="card-body">
               <p class="card-text text-truncate"><router-link class="text-link" :to="{ name: 'Detail', params: { id: whiskey.id }}">{{whiskey.제품명}}</router-link></p>

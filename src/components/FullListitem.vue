6b627046592e16c5d10deb6a1df00a363326a67b
whiscovery <whiscovery@gmail.com>
2021-05-31 16:35:29 +0900

    .

--- src/components/FullListitem.vue
+++ src/components/FullListitem.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row">
     <div class="col-sm-9">
-      <p class="text-link"><router-link class="text-link" :to="{ name: 'Detail', params: { id: whiskey.id }}">{{whiskey.제품명}}</router-link></p>
+      <p class="text-link"><router-link class="text-link" :to="{ name: 'Detail', params: { id: whiskey.id }}">• {{whiskey.제품명}}</router-link></p>
     </div>
     <div class="col-sm-3">
       <small class="list-small">{{whiskey.종류}}</small>

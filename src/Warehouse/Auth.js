ff0992a72e53bd9f8a2d6aef69a9704a04b9a305
whiscovery <whiscovery@gmail.com>
2021-06-02 18:41:46 +0900

    .

--- src/Warehouse/Auth.js
+++ src/Warehouse/Auth.js
@@ -22,6 +22,45 @@ const getters = {
     error: state => state.error
 }
 
+const mutations = {
+    auth_request (state) {
+        state.error = null
+        state.status = 'loading'
+    },
+    auth_success (state, token, user) {
+        state.token = token
+        state.user = user
+        state.status = 'success'
+        state.error = null
+    },
+    auth_error (state, err) {
+        state.error = err.response.data.msg
+    },
+    register_request (state) {
+        state.error = null
+        state.status = 'loading'
+    },
+    register_success (state) {
+        state.error = null
+        state.status = 'success'
+    },
+    register_error (state, err) {
+        state.error = err.response.data.msg
+    },
+    logout (state) {
+        state.error = null
+        state.status = ''
+        state.token = ''
+        state.user = ''
+    },
+    profile_request (state) {
+        state.status = 'loading'
+    },
+    user_profile (state, user) {
+        state.user = user
+    }
+}
+
 const actions = {
     // Login Action
     async login ({
@@ -79,44 +118,6 @@ const actions = {
     }
 }
 
-const mutations = {
-    auth_request (state) {
-        state.error = null
-        state.status = 'loading'
-    },
-    auth_success (state, token, user) {
-        state.token = token
-        state.user = user
-        state.status = 'success'
-        state.error = null
-    },
-    auth_error (state, err) {
-        state.error = err.response.data.msg
-    },
-    register_request (state) {
-        state.error = null
-        state.status = 'loading'
-    },
-    register_success (state) {
-        state.error = null
-        state.status = 'success'
-    },
-    register_error (state, err) {
-        state.error = err.response.data.msg
-    },
-    logout (state) {
-        state.error = null
-        state.status = ''
-        state.token = ''
-        state.user = ''
-    },
-    profile_request (state) {
-        state.status = 'loading'
-    },
-    user_profile (state, user) {
-        state.user = user
-    }
-}
 
 export default {
     state,

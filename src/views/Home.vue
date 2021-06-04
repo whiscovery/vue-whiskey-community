ee55e6641f3572011ad2da981aa598f200cda49a
whiscovery <whiscovery@gmail.com>
2021-06-04 22:40:33 +0900

    Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,8 @@
             <p class="title">Recent Whiskeys</p>
             <div class="wrapper m-1 p-1" v-for="whiskey, j in getWhiskeysList()" :key="j">
                 <div class="row mb-1">
-                        <div class="col-md-12 col-sm-12 md-3 mt-2"><img :src="barrel" width="100%" class="over-img m-1 me-1">
-                             <button type="button" class="btn btn-outline-warning mt-2 ms-3"><router-link class="text-link" :to="{ name: 'Detail', params: { postId: whiskey.id }}"><span class="whiskey">{{whiskey.제품명}}</span></router-link> </button>
+                        <div class="col-md-12 col-sm-12 md-3 mt-2"><img :src="barrel" class="barrel-img m-1 me-1">
+                             <button type="button" class="btn btn-outline-warning ms-3"><router-link class="text-link" :to="{ name: 'Detail', params: { postId: whiskey.id }}"><span class="whiskey">{{whiskey.제품명}}</span></router-link> </button>
                             <!-- <span class="comment-content"> "{{comment.내용}}" <small>({{comment.일시}} @ {{comment.장소}}에서)</small> </span> -->
                         </div>
                 </div>
@@ -126,8 +126,13 @@ export default {
   font-size: 13px;
     font-family: 'Exo 2', sans-serif;
 }
+.barrel-img{
+    width: 50px;
+  position: relative;
+  opacity: 0.0.8; filter: alpha(opacity=50);
+}
 .over-img{
-    width: 40px;
+    width: 50px;
   position: relative;
   top: 3px;
   left: 3px;
